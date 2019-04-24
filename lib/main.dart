e4a91ba2243c6fd9a1083718a96e3fe01b08fc56
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-04-24 11:04:49 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HalamanHalo(),
+  ));
+}
+
+class HalamanHalo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello World')
+    );
+  }
+}
